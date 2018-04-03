df428d031abc6c816eb5f292067ef2ddd8634194
Vladimir <idunerg@gmail.com>
2018-04-03 14:18:12 +0000

    now it is done, realy

--- src/index.js
+++ src/index.js
@@ -12,20 +12,21 @@ const config = {
 const env = config.heroku;
 
 const domain = env.domain;
-const port = 8080;
+
 let IP;
 
 //Looper path if no IP to redirect to , a client will wait untill it will appear
 app.get("",(req,res)=>{
     if(!IP){
         console.log("No IP to redirect to. Client is WAITING...");
-        setTimeout(()=>{
-            res.set("Location",`http://${IP || domain}:${port}`);
+        setTimeout(()=>{ 
+            res.set("Location",`http://${domain}/?port=${req.query.port || 8080}`);
             res.status(303);
             res.end();
-            console.log("Redirected to try again to:",`http://${IP || domain}:${port}`);
+            console.log("Redirected to try again to:",`http://${domain}/?port=${req.query.port || 8080}`);
         },3000);
     }else{
+        const port = req.query.port || 8080;
         res.set("Location",`http://${IP}:${port}`);
         res.status(303);
         res.end();
